4b20db17c54d391a305058b3da9f8eb8ce0daf57
Alexandre Merancienne <alexandre.merancienne@gmail.com>
2021-06-18 16:02:25 +0300

    CRUD endpoints added for issues and comments

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,21 @@
+from django.conf.urls import url
+from django.urls import include
 from rest_framework.routers import SimpleRouter
+from rest_framework_nested import routers
 
-from .views import ProjectViewSet
+from api.views import ProjectViewSet, IssueViewSet, CommentViewSet
 
 router = SimpleRouter()
+router.register(r'projects', ProjectViewSet, basename='projects')
 
-router.register('projects', ProjectViewSet, basename='projects')
+projects_router = routers.NestedSimpleRouter(router, r'projects', lookup='project')
+projects_router.register(r'issues', IssueViewSet, basename='issues')
 
-urlpatterns = router.urls
+issues_router = routers.NestedSimpleRouter(projects_router, r'issues', lookup='issue')
+issues_router.register(r'comments', CommentViewSet, basename='comments')
+
+urlpatterns = [
+    url(r'^', include(router.urls)),
+    url(r'^', include(projects_router.urls)),
+    url(r'^', include(issues_router.urls)),
+]
--- api/views.py
+++ api/views.py
@@ -1,9 +1,9 @@
 from rest_framework import viewsets
 from rest_framework.decorators import action
 
-from todos.models import Project
+from todos.models import Project, Issue, Comment
 from rest_framework import permissions
-from .serializers import ProjectSerializer
+from .serializers import ProjectSerializer, IssueSerializer, CommentSerializer
 
 
 class ProjectViewSet(viewsets.ModelViewSet):
@@ -11,7 +11,28 @@ class ProjectViewSet(viewsets.ModelViewSet):
     queryset = Project.objects.all()
     serializer_class = ProjectSerializer
 
-    @action(detail=True, methods=['get'])
+    @action(detail=True)
     def get_queryset(self):
         user = self.request.user
         return Project.objects.filter(author=user)
+
+
+class IssueViewSet(viewsets.ModelViewSet):
+    permission_classes = (permissions.IsAuthenticated,)
+    serializer_class = IssueSerializer
+    queryset = Issue.objects.all()
+
+    @action(detail=True)
+    def get_queryset(self):
+        return Issue.objects.filter(project=self.kwargs['project_pk'])
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    permission_classes = (permissions.IsAuthenticated,)
+    serializer_class = CommentSerializer
+    queryset = Comment.objects.all()
+
+    @action(detail=True)
+    def get_queryset(self):
+        return Comment.objects.filter(issue=self.kwargs['issue_pk'])
+
